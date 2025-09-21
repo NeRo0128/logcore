39db4398a9fd6f03013979818996756baf2769ee
¨Nero¨ <castillovegarodolfogilberto@gmail.com>
2025-09-21 17:49:38 -0400

    Agregar archivos go.mod, go.sum y logs.go para la gestión de dependencias y funciones de registro

---
+++ logs.go
@@ -0,0 +1,21 @@
+package logcore
+
+import "Repositories/logcore/logger"
+
+var defaultLogger = logger.NewLogger()
+
+func LogSuccess(msg string) {
+	defaultLogger.Info(msg)
+}
+
+func LogInfo(msg string) {
+	defaultLogger.Info(msg)
+}
+
+func LogWarning(msg string) {
+	defaultLogger.Warn(msg)
+}
+
+func LogError(msg string) {
+	defaultLogger.Error(msg)
+}
